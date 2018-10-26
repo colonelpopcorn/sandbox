18aceaddf4e4c3974e920561985ab14720a6bd17
Jonathan Ling <colonelpopcorn.jl@gmail.com>
2018-10-25 21:46:27 -0400

    Add initial generation of angular app.

---
+++ js/angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoundInputComponent } from './bound-input/bound-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoundInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ js/angular/src/app/bound-input/bound-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bound-input',
+  templateUrl: './bound-input.component.html',
+  styleUrls: ['./bound-input.component.scss']
+})
+export class BoundInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
