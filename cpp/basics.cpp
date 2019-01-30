3b87d7860a51d3714fae39ef180f65bb86e13218
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-01-30 07:58:27 -0500

    Add a class to the main file.

--- cpp/basics.cpp
+++ cpp/basics.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <string>
+
+class Greeter {
+    std::string Name;
+    public:
+        Greeter(std::string Name) { this->Name = Name; }
+        std::string getName() { return this->Name; }
+        void greet() {
+            std::string finalStr = std::string("Hello ") + this->Name + std::string("!");
+            printf("%s", finalStr.c_str());
+        }
+};
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello, world!");
+    printf("Hello, world!\n");
+    Greeter greeter("Jonathan");
+    greeter.greet();
     return 0;
 }
