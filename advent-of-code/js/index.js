4233faacf3513bc926138e35a1de5b5763be12c1
colonelpopcorn <colonelpopcorn.jl@gmail.com>
2021-09-03 19:34:41 +0000

    Get output printer for all days.

--- advent-of-code/js/index.js
+++ advent-of-code/js/index.js
@@ -4,6 +4,6 @@ const days = require('./days');
 const day = process.argv[2] || '';
 if (day === '' || days[day] === undefined) {
   console.error("Please pass a valid day!")
+  return 2;
 }
-console.log(days[day]);
 console.log(days[day]());
