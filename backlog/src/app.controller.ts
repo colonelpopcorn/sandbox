fe328a19abaf146444637c40c826c9263b8de69d
Jonathan Ling <jonathanling@shamrocksweb.com>
2021-12-15 15:42:48 -0500

    WIP - trying to get MVC working.

--- backlog/src/app.controller.ts
+++ backlog/src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+
+import { Get, Controller, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return { message: 'Hello world!' };
   }
 }
